91c627e934f1d288c91878751dd6c952c219d3d0
Mohamed Aberkani <aberkanimed@gmail.com>
2021-06-27 16:23:05 +0200

    Upload clean code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,19 @@
 import Auth from './Modules/Auth/Auth.js';
 import Asistencia from './Modules/Asistencia.js';
+import Tarea from './Modules/Tarea.js';
+import Pildora from './Modules/Pildora.js';
 
 const btnListadoFirmas = document.getElementById('btnListadoFirmas');
 const btnFirmar = document.getElementById('btnFirmar');
 const btnNumFirmasHoy = document.getElementById('btnNumFirmasHoy');
+const btnListadoTareas = document.getElementById('btnListadoTareas');
+const btnListadoPildoras = document.getElementById('btnListadoPildoras');
 
 btnListadoFirmas.addEventListener('click', getListadoFirmas, false);
 btnNumFirmasHoy.addEventListener('click', getNumFirmasHoy, false);
 btnFirmar.addEventListener('click', firmarAsistencia, false);
+btnListadoTareas.addEventListener('click', getListadoTareas, false);
+btnListadoPildoras.addEventListener('click', getListadoPildoras, false);
 
 async function getListadoFirmas() {
     let res = await Asistencia.getlistadoFirmas();
@@ -26,5 +32,15 @@ async function firmarAsistencia() {
         estado: 1
     }
     let res = await Asistencia.firmar(firma);
+    console.log(res);
+}
+
+async function getListadoTareas() {
+    let res = await Tarea.getListadoTareas();
+    console.log(res);
+}
+
+async function getListadoPildoras() {
+    let res = await Pildora.getListadoPildoras();
     console.log(res);
 }
